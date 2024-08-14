80ffdeb9bf46b10b38c508ab519a434a47c98f8e
Sarvesh Khamkar <sarveshkhamkar321@gmail.com>
2024-08-14 07:07:33 +0530

    Dom Completed

--- Dom/dom.js
+++ Dom/dom.js
@@ -4,4 +4,7 @@ window object represent a open window inside the browser
 all html is present inside window
 */
 
-console.log(document.body)
+let btn = document.createElement("button")
+btn.innerText="Click-me"
+btn.style.color ="light-green"
+document.querySelector('body').prepend(btn)
